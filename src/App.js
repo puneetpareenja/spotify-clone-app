920c3dd82ee5094354bdcf5b2c01874952d2e5de
Puneet Pareenja <pareenjapuneet@gmail.com>
2020-10-21 14:42:19 -0700

    Created Login Component, authentication with Spotify

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
-import React from "react";
-import "./App.css";
+import React, { useEffect, useState } from "react";
+import "./css/App.css";
+import Login from "./components/Login";
+import { getTokenFromResponse } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from "./components/Player";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
-  return <div className="app">Spotify Clone</div>;
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getTokenFromResponse();
+    window.location.hash = "";
+    console.log(hash);
+
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then((user) => {
+        console.log("user", user);
+      });
+    }
+  }, []);
+
+  return <div className="app">{token ? <Player /> : <Login />}</div>;
 }
 
 export default App;
---
+++ src/components/Player.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Player() {
+  return <div>Player component</div>;
+}
+
+export default Player;
